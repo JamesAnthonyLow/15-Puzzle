8861e0bb29d783ba63326fda7f944f8a7b23d8e9
JamesAnthonyLow <jamesalowenthal@gmail.com>
2017-02-15 13:51:30 +0000

    add MoveTiles to Keys.listen conditional on spacebar

--- public/js/src/Keys.js
+++ public/js/src/Keys.js
@@ -2,11 +2,6 @@ var Keys = (function(){
   var spacebarPressed = false;
   return {
     listen: function(){
-      var movecursor, hit = false;
-      var arrowkey = function(e, mvcursor){
-        e.preventDefault();
-        mvcursor();
-      };
       window.onkeyup = function(e){
         if(e.keyCode == 32){
           e.preventDefault();
@@ -14,6 +9,13 @@ var Keys = (function(){
         }
       };
       window.onkeydown = function(e){
+        var movecursor, movetiles, hit = false;
+        var arrowkey = function(e, mvcursor, mvtiles){
+          e.preventDefault();
+          if(spacebarPressed)
+            mvtiles();
+          mvcursor();
+        };
         switch(e.keyCode){
           case 32:
             e.preventDefault();
@@ -21,23 +23,28 @@ var Keys = (function(){
             break;
           case 37:
             movecursor = function(){ Cursor.moveCursorOneLeft(); };
+            movetiles = function(){ Cursor.moveTilesLeft(); };
             hit = true;
             break;
           case 38:
             movecursor = function(){ Cursor.moveCursorOneUp(); };
+            movetiles = function(){ Cursor.moveTilesUp(); };
             hit = true;
             break;
           case 39:
             movecursor = function(){ Cursor.moveCursorOneRight(); };
+            movetiles = function(){ Cursor.moveTilesRight(); };
             hit = true;
             break;
           case 40:
             movecursor = function(){ Cursor.moveCursorOneDown(); };
+            movetiles = function(){ Cursor.moveTilesDown(); };
             hit = true;
             break;
         }
-        if(hit)
-          arrowkey(e, movecursor);
+        if(hit){
+          arrowkey(e, movecursor, movetiles);
+        }
       };
     }
   };
