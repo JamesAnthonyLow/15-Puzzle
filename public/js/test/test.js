faf50ce42017a883aec488b70a3a5672c55b2c51
JamesAnthonyLow <jamesalowenthal@gmail.com>
2017-02-15 05:03:42 +0000

    refactor tests for greater OO

--- public/js/test/test.js
+++ public/js/test/test.js
@@ -1,51 +1,51 @@
-function RunTests(){
-  //helpers
-  var fail = function(message){
+var Helpers  = {
+  fail: function(message){
     console.error("%c FAIL " + message, "color: red");
     alert("FAIL " + message);
-  };
-  var pass = function(message){
+  },
+  pass: function(message){
     console.log("%c PASS " + message, "color: blue");
-  };
-  var expect = function(message, bool){
+  },
+  expect: function(message, bool){
     if(bool)
-      pass(message);
+      this.pass(message);
     else
-      fail(message);
-  };
+      this.fail(message);
+  }
+};
 
-  var Tests = {
-    test_getChosen: function(name){
-      var chosen = getChosen();
-      expect(name, chosen !== undefined);
-    },
-    test_getEmpty: function(name){
-      var empty = getEmpty();
-      expect(name, empty !== undefined);
-    },
-    test_newPiece: function(name){
-      var chosen = document.getElementsByClassName("chosen")[0];
-      var piece = newPiece(chosen);
-      expect(name + " has top",    piece.top    !== undefined);
-      expect(name + " has left",   piece.left   !== undefined);
-      expect(name + " has right",  piece.right  !== undefined);
-      expect(name + " has bottom", piece.bottom !== undefined);
-    },
-    test_getPieceHeight: function(name){
-      var height = getPieceHeight();
-      expect(name + " height is defined", height !== undefined);
-      expect(name + " height is greater than 0", height > 0);
-    },
-    test_getPieceWidth: function(name){
-      var width = getPieceWidth();
-      expect(name + " width is defined", width !== undefined);
-      expect(name + " width is greater than 0", width > 0);
-    }
-  };
-
-  //run each test
+function RunTests(TestObj){
+   //run each test
   console.clear();
-  for (var key in Tests) 
-    Tests[key](key.toString());
+  for (var key in TestObj) 
+    TestObj[key](key.toString());
 }
 
+var TestPiece = {
+  test_getChosen: function(name){
+    var chosen = Piece.getChosen();
+    Helpers.expect(name, chosen !== undefined);
+  },
+  test_getEmpty: function(name){
+    var empty = Piece.getEmpty();
+    Helpers.expect(name, empty !== undefined);
+  },
+  test_newPiece: function(name){
+    var chosen = document.getElementsByClassName("chosen")[0];
+    var piece = Piece.newPiece(chosen);
+    Helpers.expect(name + " has top",    piece.top    !== undefined);
+    Helpers.expect(name + " has left",   piece.left   !== undefined);
+    Helpers.expect(name + " has right",  piece.right  !== undefined);
+    Helpers.expect(name + " has bottom", piece.bottom !== undefined);
+  },
+  test_getPieceHeight: function(name){
+    var height = Piece.getPieceHeight();
+    Helpers.expect(name + " height is defined", height !== undefined);
+    Helpers.expect(name + " height is greater than 0", height > 0);
+  },
+  test_getPieceWidth: function(name){
+    var width = Piece.getPieceWidth();
+    Helpers.expect(name + " width is defined", width !== undefined);
+    Helpers.expect(name + " width is greater than 0", width > 0);
+  }
+};
