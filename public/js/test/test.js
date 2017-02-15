8d4b5165a6fc05487c8ffaea0ab1bc3792319d68
JamesAnthonyLow <jamesalowenthal@gmail.com>
2017-02-15 04:45:02 +0000

    add constructor for Piece object

--- public/js/test/test.js
+++ public/js/test/test.js
@@ -23,26 +23,23 @@ function RunTests(){
       var empty = getEmpty();
       expect(name, empty !== undefined);
     },
-    test_getPosition: function(name){
-      var chosen = getChosen();
-      var pos = getPosition(chosen);
-      expect(name + " returns obj", 
-        pos !== undefined);
-      if(pos === undefined)
-        return;
-      expect(name + " obj has top element", 
-        pos.top !== undefined);
-      expect(name + " obj has left element", 
-        pos.left !== undefined);
-      expect(name + " obj has right element", 
-        pos.right !== undefined);
-      expect(name + " obj has bottom element", 
-        pos.bottom !== undefined);
+    test_newPiece: function(name){
+      var chosen = document.getElementsByClassName("chosen")[0];
+      var piece = newPiece(chosen);
+      expect(name + " has top",    piece.top    !== undefined);
+      expect(name + " has left",   piece.left   !== undefined);
+      expect(name + " has right",  piece.right  !== undefined);
+      expect(name + " has bottom", piece.bottom !== undefined);
     },
     test_getPieceHeight: function(name){
       var height = getPieceHeight();
       expect(name + " height is defined", height !== undefined);
       expect(name + " height is greater than 0", height > 0);
+    },
+    test_getPieceWidth: function(name){
+      var width = getPieceWidth();
+      expect(name + " width is defined", width !== undefined);
+      expect(name + " width is greater than 0", width > 0);
     }
   };
 
